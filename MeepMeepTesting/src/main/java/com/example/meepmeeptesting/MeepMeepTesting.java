4c08bf3e93396ab8f4d0ff181f42fe1fbcf6541c
peterultraman <phan@assets-school.org>
2026-09-15 09:13:18 -1000

    Updated to v12

---
+++ MeepMeepTesting/src/main/java/com/example/meepmeeptesting/MeepMeepTesting.java
@@ -0,0 +1,35 @@
+package com.example.meepmeeptesting;
+
+
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.noahbres.meepmeep.MeepMeep;
+import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
+import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
+
+
+public class MeepMeepTesting {
+    public static void main(String[] args) {
+        MeepMeep meepMeep = new MeepMeep(700);
+        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(50, 50, Math.toRadians(180), Math.toRadians(180), 14.5)
+                .build();
+        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(60, -14, Math.toRadians(180)))
+                // Run your test paths here
+                //...
+                //Examples below
+                //.strafeTo(new Vector2d(0, 0))
+                //.strafeToLinearHeading(new Vector2d(0,0), Math.toRadians(0))
+                //.splineTo(new Vector2d(0, 0), Math.toRadians(0))
+                //.splineToConstantHeading(new Vector2d(0,0), Math.toRadians(0))
+                //.splineToLinearHeading(new Pose2d(0,0, Math.toRadians(0)), Math.toRadians(0))
+                //.waitSeconds(0.5)
+                .build());
+        meepMeep.setBackground(MeepMeep.Background.FIELD_DECODE_OFFICIAL)
+                .setDarkMode(true)
+                .setBackgroundAlpha(0.95f)
+                .addEntity(myBot)
+                .start();
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutoTemplate.java
@@ -0,0 +1,74 @@
+//Auto Template that you can use to program your auto
+// NOTE: THIS CODE RUNS ON ODOMETRY, you must have your odometry tuned
+
+package org.firstinspires.ftc.teamcode;
+
+import com.acmerobotics.roadrunner.Action;
+import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.SequentialAction;
+import com.acmerobotics.roadrunner.Vector2d;
+import com.acmerobotics.roadrunner.ftc.Actions;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.odometry.MecanumDrive;
+
+@Autonomous
+public class AutoTemplate extends LinearOpMode {
+
+    //Include all of your actuators
+    Actuators actuator = new Actuators();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        //Include all of your motors hardware
+        actuator.init(hardwareMap);
+
+        //TODO: Set up Robot starting position here!!!
+        MecanumDrive drive = new MecanumDrive(hardwareMap,
+                new Pose2d(0, 0, Math.toRadians(90) ) );
+
+        //TODO: Put your Odometry trajectory paths here!!!
+        //Separate your paths by action (linearSlide movements, intake movements, servo movements, etc.)
+        //Work shop it line 35,36
+        Action trajectory1 = drive.actionBuilder(new Pose2d(0, 0, Math.toRadians(90) ))
+                .strafeToLinearHeading(new Vector2d( -42, 32), Math.toRadians(-52))
+                .strafeToLinearHeading(new Vector2d( 34, 1), Math.toRadians(93))
+                .strafeToLinearHeading(new Vector2d( 0, 0), Math.toRadians(90))
+                //...
+                .build();
+
+	    //TODO: Your trajectory 2 starting pose must match where the end of your trajectory 1 should be
+        Action trajectory2 = drive.actionBuilder(new Pose2d(-34, -29, Math.toRadians(127) ))
+                .strafeToLinearHeading(new Vector2d(-23, -23), Math.toRadians(90))
+                //...
+                .build();
+
+	    //TODO: Your trajectory 3 starting pose must match where the end of your trajectory 2 should be
+        Action trajectory3 = drive.actionBuilder(new Pose2d(0, 0, Math.toRadians(90) ))
+                .strafeToLinearHeading(new Vector2d(-40,-30), Math.toRadians(-180))
+                .build();
+
+        Action trajectory4 = drive.actionBuilder(new Pose2d(-40, -30, Math.toRadians(-180) ))
+                .strafeToLinearHeading(new Vector2d(0,0), Math.toRadians(90))
+                .build();
+        //Create more trajectories as needed
+
+        while (!isStarted() && !isStopRequested()) {
+            //include all of your hardware configurations
+            actuator.init(hardwareMap);
+        }
+
+        waitForStart();
+
+        if (opModeIsActive() && !isStopRequested()) {
+            //Run your trajectory here when the code starts
+          Actions.runBlocking(new SequentialAction(trajectory1));
+            //actuator.setServoPosition(0.44); //close servo
+            //...
+            //...
+            //...
+        }
+    }
+
+}
